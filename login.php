575eac331f24baed85b1778cba769b04e24b045e
Unknown <pulsarlens@gmail.com>
2018-02-07 15:18:16 -0600

    Added files

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="styles.css">
+    <title>TAS Login</title>  
+</head>
+<body>
+    <div class="login-container">
+        <h3>Ingresa tus Credenciales</h3>
+        <form action="">
+            <label for="usrname">Usuario</label>
+            <input type="text" id="usrname" name="usrname" placeholder="Ingresa tu nombre de usuario">
+            <label for="usrname">Contrase&ntilde;a</label>
+            <input type="text" id="usrname" name="usrname" placeholder="Ingresa tu nombre de usuario">
+            <input type="submit" value="Ingresar">
+        </form>
+    </div>
+</body>
+</html>
