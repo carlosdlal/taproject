d39a16593b5cd45e181f320b08e5ba8f8a64c39f
unknown <pulsarlens@gmail.com>
2018-02-16 11:23:58 -0600

    Several Changes

--- materials.php
+++ materials.php
@@ -5,9 +5,49 @@
         <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
     </head>
     <body>
+        <div class="table-content">
+            <form action="">
+                <table class="inner-table">
+                    <tr>
+                        <th><label for="material">Material</label></th>
+                        <th><label for="subtype">Subtipo</label></th>
+                        <th><label for="figure">Figura</label></th>
+                        <th><label for="x1">Medida 1</label></th>
+                        <th><label for="x2">Medida 2</label></th>
+                        <th><label for="x3">Medida 3</label></th>
+                        <th><label for="price">Precio</label></th>
+                        <th><label for="quantity">Cantidad</label></th>
+                    </tr>
+                    <tr>
+                        <td>
+                            <select>
+                                <option value="acero">Acero</option>
+                                <option value="nylamid">Nylamid</option>
+                                <option value="aluminio">Aluminio</option>
+                                <option value="bronce">Bronce</option>
+                            </select>
+                        </td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </table>
+            </form>  
+        </div>
         <div class="material-container">
+            <div class="materials-title">
+                <h3>MATERIAL</h3>
+            </div>
+            <div class="shadow-text">
+                <p>Comienza dando click en el simbolo "+"</p>
+            </div>
             <div class="add-btn">
                 <i class="fas fa-plus font-center"></i>
             </div>
+            
     </body>
 </html>
