d3232aa9b6839b249e990fa1f495d2eec32f04cb
Unknown <pulsarlens@gmail.com>
2018-02-19 11:46:06 -0600

    Several form changes

--- admin.js
+++ admin.js
@@ -2,10 +2,14 @@ $( document ).ready(function() {
  
     $material = $("select[name='material']");
     $subtype = $("select[name='subtype']");
-    $figure = $("select[figure]");
+    $figure = $("select[name='figure']");
+    $x1_metric=$("select[name='x1-metric']");
+    $x1_value=$("input[name='x1-value']"); 
+
     $material.change(function(){
         if($(material).val()=="Acero"){
             $("select[name='subtype'] option").remove();
+            $("<option disabled selected value> Tipo de Acero</option>").appendTo($subtype);
             $("<option value='1018'>1018</option>").appendTo($subtype);
             $("<option value='1045'>1045</option>").appendTo($subtype);
             $("<option value='tratado'>4140 Tratado</option>").appendTo($subtype);
@@ -13,6 +17,7 @@ $( document ).ready(function() {
             $("<option value='d2'>D2</option>").appendTo($subtype);
             $("<option value='bh'>Barra Hueca</option>").appendTo($subtype);
             $("<option value='o1'>O-1</option>").appendTo($subtype);
+            $("<option value='12L14'>12L14</option>").appendTo($subtype);
         }
         if($(this).val()=="Aluminio"){
             $("select[name='subtype'] option").remove();
@@ -40,11 +45,84 @@ $( document ).ready(function() {
     });
 
     $subtype.change(function(){
-        if($(material).val()=="Acero"){
-            
+        $( ".price" ).hide();
+        if($(subtype).val()!="bh" && $(material).val()=="Acero"){
+            $( ".figure" ).show();
+            $("select[name='figure'] option").remove();
+            $("<option disabled selected value>Forma</option>").appendTo($figure);
+            $("<option value='round'>Redondo</option>").appendTo($figure);
+            $("<option value='plate'>Solera</option>").appendTo($figure);
+            $("<option value='square'>Cuadrado</option>").appendTo($figure);
+            if($(subtype).val()=="1018"||$(subtype).val()=="12L14"){
+                $("<option value='hex'>Hexagonal</option>").appendTo($figure);
+            }
+        }
+        else{
+            $( ".price" ).show();
+            $( ".figure" ).hide();
+            $( ".x1" ).show();
+            $( ".x2" ).show();
+            $( ".x3" ).show();
+            $("#lblx1").empty();
+            $("#lblx1").append("DIAMETRO INTERNO");
+            $("#lblx2").empty();
+            $("#lblx2").append("DIAMETRO EXTERNO");
+            $("#lblx3").empty();
+            $("#lblx3").append("LARGO");
         }
     });
 
+    $figure.change(function(){
+        $( ".price" ).show();
+        if($(figure).val()=="round"){
+            $( ".x1" ).show();
+            $( ".x2" ).show();
+            $( ".x3" ).hide();
+            $("#lblx1").empty();
+            $("#lblx1").append("DIAMETRO");
+            $("#lblx2").empty();
+            $("#lblx2").append("LARGO");
+        }
+        if($(figure).val()=="plate"){
+            $( ".x1" ).show();
+            $( ".x2" ).show();
+            $( ".x3" ).show();
+            $("#lblx1").empty();
+            $("#lblx1").append("ESPESOR");
+            $("#lblx2").empty();
+            $("#lblx2").append("ANCHO");
+            $("#lblx3").empty();
+            $("#lblx3").append("LARGO");
+        }
+        if($(figure).val()=="square" || $(figure).val()=="hex"){
+            $( ".x1" ).show();
+            $( ".x2" ).show();
+            $( ".x3" ).hide();
+            $("#lblx1").empty();
+            $("#lblx1").append("LADO");
+            $("#lblx2").empty();
+            $("#lblx2").append("LARGO");
+        }
+        if($(figure).val()=="buje"){
+            $( ".x1" ).show();
+            $( ".x2" ).show();
+            $( ".x3" ).show();
+            $("#lblx1").empty();
+            $("#lblx1").append("DIAMETRO INTERNO");
+            $("#lblx2").empty();
+            $("#lblx2").append("DIAMETRO EXTERNO");
+            $("#lblx3").empty();
+            $("#lblx3").append("LARGO");
+        }
 
+    });
+
+    $x1_value.change(function(){
+        if($(x1_metric).val()=="mts" ){
+            alert("Hola");
+        }
+
+    });
+    
 
 });
